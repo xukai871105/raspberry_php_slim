2d8775c677a8f98b121f0350722fad7106f19c6e
xukai <xukai19871105@126.com>
2014-03-04 20:34:12 +0800

    增加说明

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ raspberry_php_slim
 ==================
 
 树莓派上使用PHP slim框架，应用REST API
+
+1.打开conn.php 修改数据库用户名和连接密码
+2.在指定的数据库中导入leds.sql,创建leds表格和若干led记录
+3.更多内容，请查看博客http://blog.csdn.net/xukai871105/article/details/18775957
+      
+
--- conn.php
+++ conn.php
@@ -1,9 +1,10 @@
 <?php
-
-$conn = @mysql_connect('127.0.0.1','root','871105');
+// 修改用户名和密码
+$conn = @mysql_connect('127.0.0.1','root','123456');
 if (!$conn) {
 	die('Could not connect: ' . mysql_error());
 }
+// 修改数据库名称
 mysql_select_db('mysql', $conn);
 
 ?>
